8b958d33ddb376dc9958b1261b649c81109418c8
Gianluca Illiano <gianluca.illiano@smartly.io>
2021-12-03 00:03:19 +0200

    Solve day2 part1 challenge

---
+++ 2021/02/main.c
@@ -0,0 +1,34 @@
+#include "stdio.h"
+#include "string.h"
+
+int main() {
+    FILE *file = fopen("input.txt", "r");
+    if (file == NULL) {
+        return 1;
+    }
+
+    int x = 0;
+    int y = 0;
+    
+    char line[32] = "";
+    while(fgets(line, sizeof(line), file)) {
+        char command[32] = "";
+        int movement;
+        sscanf(line, "%s %d", command, &movement);
+
+        if (strcmp(command, "forward") == 0) {
+            x += movement;
+        } else if (strcmp(command, "up") == 0) {
+            y -= movement;
+        } else if (strcmp(command, "down") == 0) {
+            y += movement;
+        } else {
+            return 1;
+        }
+    }
+
+    printf("Sumbarine position X:%d Y:%d Mul: %d", x, y, x * y);
+    
+    fclose(file);
+    return 0;
+}
