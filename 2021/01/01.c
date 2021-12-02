376ac470aa6dec1638a51813ce4408d390cca61b
Gianluca Illiano <gianluca.illiano@smartly.io>
2021-12-02 23:35:37 +0200

    Solve day1 part2 challenge

--- 2021/01/01.c
+++ 2021/01/01.c
@@ -1,11 +1,6 @@
 #include "stdio.h"
 
-int main() {
-    FILE *file = fopen("input.txt", "r");
-    if (file == NULL) {
-        return 1;
-    }
-
+void part1(FILE *file) {
     int increments = 0;
     char line[16] = "";
 
@@ -29,5 +24,53 @@ int main() {
     }
 
     printf("Depth increased %d times!\n", increments);
+}
+
+void part2(FILE *file) {
+    int increments = 0;
+    char line[16] = "";
+
+    int groupSize = 3;
+
+    int *prev = NULL;
+    int prevGroup[groupSize];
+    int i = 0;
+    while(fgets(line, sizeof(line), file)) {
+        i = i % groupSize;
+
+        int curr;
+        sscanf(line, "%d", &curr);
+
+        prevGroup[i] = curr;
+        if (i == groupSize - 1 && prev == NULL) {
+            int value = prevGroup[0] + prevGroup[1] + prevGroup[2];
+            prev = &value;
+        } else if (prev != NULL) {
+            int value = prevGroup[0] + prevGroup[1] + prevGroup[2];
+            if (value > *prev) {
+                increments++;
+            }
+
+            *prev = value;
+        }
+
+        i++;
+    }
+
+    printf("Depth increased %d times!\n", increments);
+}
+
+int main() {
+    FILE *file = fopen("input.txt", "r");
+    if (file == NULL) {
+        return 1;
+    }
+
+    part1(file);
+    fseek(file, 0, SEEK_SET);
+
+    part2(file);
+
+    fclose(file);
     return 0;
 }
